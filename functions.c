cd244833d0c01eeb18320ff1625f2aac3f9f1d1a
MikeRock51 <mikerockmusic51@gmail.com>
2022-10-17 08:44:13 -0700

    Updating code

---
+++ functions.c
@@ -0,0 +1,113 @@
+#include "main.h"
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+/**
+ * _putchar - Prints a character
+ * @c: Character to be printed
+ *
+ * Return: c as passed
+ */
+char _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
+/**
+ * _strlen - Checks the length of a string
+ * @c: The string to be checked
+ *
+ * Return: The length of c
+ */
+int _strlen(const char *c)
+{
+	int i = 0;
+
+	while (c[i])
+	i++;
+	return (i);
+}
+
+/**
+ * print_char - Prints a character
+ * @arg: Character to print
+ *
+ * Return: length of char
+ */
+int print_char(va_list arg)
+{
+	char character;
+
+	character = va_arg(arg, int);
+	_putchar(character);
+	return (1);
+}
+
+/**
+ * print_int - Prints integers
+ * @arg: Integer to print
+ *
+ * Return: Length of arg
+ */
+int print_int(va_list arg)
+{
+	int numb, expo = 1, len = 0;
+	unsigned int num;
+	char ch;
+
+	numb = va_arg(arg, int);
+
+	if (numb < 0)
+	{
+		_putchar('-');
+		numb = -numb;
+	}
+
+	num = numb;
+
+	while (num / expo > 9)
+		expo *= 10;
+
+	while (expo != 0)
+	{
+		ch = num / expo + '0';
+		len = len + write(1, &ch, 1);
+		num = num % expo;
+		expo = expo / 10;
+	}
+	return (len);
+}
+
+/**
+ * print_string - Prints a string
+ * @arg: String to print
+ *
+ * Return: Length of the string
+ */
+int print_string(va_list arg)
+{
+	char *str;
+	int i;
+
+	str = va_arg(arg, char *);
+	for (i = 0; i < str[i]; i++)
+	{
+		_putchar(str[i]);
+	}
+	return(i);
+}
+
+/**
+ * print_percent - Prints the percent sign
+ * @arg: Unused argument
+ *
+ * Return: Length of char
+ */
+int print_percent(va_list arg)
+{
+	(void) arg;
+	_putchar('%');
+	return(1);
+}
--- printf.c
+++ printf.c
@@ -4,115 +4,50 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-char _putchar(char c)
-{
-    return (write(1, &c, 1));
-}
-
-int _strlen(const char *c)
-{
-  int i = 0;
-
-  while (c[i])
-    i++;
-  return (i);
-}
-
-int print_char(va_list arg)
-{
-  char character;
-  character = va_arg(arg, int);
-  _putchar(character);
-  return (1);
-}
 /**
-int print_int(va_list arg)
-{
-  unsigned int numb;
-  numb = va_arg(arg, int);
-
-  if (numb < 0)
-  {
-    _putchar('-');
-    numb = -numb;
-  }
-  if (numb / 10)
-  {
-    print_int(numb / 10);
-  }
-  _putchar(numb % 10 + '0');
-  return (1);
-}
-*/
-
-int print_string(va_list arg)
-{
-  char *str;
-  int i;
-
-  str = va_arg(arg, char *);
-  for (i = 0; i < str[i]; i++)
-  {
-    _putchar(str[i]);
-  }
-return(i);
-}
-
-int print_percent(va_list arg)
-{
-  (void) arg;
-  _putchar('%');
-  return(1);
-}
-
+ * _printf - Prints format string
+ * @format: Specfier to determine the the how to print arguments
+ *
+ * Return: The length of string printed
+ */
 int _printf(const char *format, ...)
 {
-  va_list args;
-  int i, j, status;
+	va_list args;
+	int i, j, length = 0;
 
-  va_start(args, format);
+	va_start(args, format);
 
-  print_spec specs[] = {
-      {'c', print_char},
-      /*{'i', print_int},
-      {'d', print_int},*/
-      {'s', print_string},
-      {'%', print_percent},
-      {'\0', NULL}
-  };
+	print_spec specs[] = {
+	{'c', print_char},
+	{'i', print_int},
+	{'d', print_int},
+	{'s', print_string},
+	{'%', print_percent},
+	{'\0', NULL}
+	};
 
-  i = 0;
-  while (format[i])
-  {
-    if (format[i] != '%')
-    {
-        write(1, &format[i], 1);
-        i++;
-    }
-    else if (format[i] == '%')
-    {
-        i++;
-        for (j = 0; specs[j].specifier != '\0'; j++)
-        {
-            if (format[i] == (specs[j].specifier))
-            {
-                i++;
-                specs[j].print(args);
-            }
-        }
-    }
-  }
-  va_end(args);
-  return (0);
-}
-
-int main(void) {
-    char *name = "Mike";
-    char initial = 'O';
-
-  _printf("Hello %s %c. %s%%\n", name, initial, "100");
-  printf("Hello %s %c. %s%%\n", name, initial, "100");
-  _printf("Let's try to printf a simple sentence.\n");
-  printf("Let's try to printf a simple sentence.\n");
-  return (0);
+	i = 0;
+	while (format[i])
+	{
+		if (format[i] != '%')
+		{
+			write(1, &format[i], 1);
+			i++;
+			length++;
+		}
+		else if (format[i] == '%')
+		{
+			i++;
+			for (j = 0; specs[j].specifier != '\0'; j++)
+			{
+				if (format[i] == (specs[j].specifier))
+				{
+					i++;
+					specs[j].print(args);
+				}
+			}
+		}
+	}
+	va_end(args);
+	return (length);
 }
