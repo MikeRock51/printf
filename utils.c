3dece8143e17066554d5763cca8db49f36500469
Abdullah Clement Abdul Shakur <abdulshakuraclement@yahoo.com>
2022-10-17 11:53:06 -0700

    added utiliy helper functions

---
+++ utils.c
@@ -0,0 +1,65 @@
+#include "main.h"
+#include <stdio.h>
+
+
+/**
+ * rev_str - reverses a string in place
+ *
+ * @s: string to reverse
+ * Return: A pointer to a character
+ */
+
+char *rev_str(char *s)
+{
+  int len;
+  int head;
+  char tmp;
+  char *dest;
+
+  for (len = 0; s[len] != '\0'; len++)
+    {
+    
+    }
+  dest = malloc(sizeof(char) * len + 1);
+  if(dest == NULL)
+    return (NULL);
+  _memcpy(dest, s, len);
+  for( head = 0; head < len; head++, len--)
+    {
+      tmp = dest[len - 1];
+      dest[len -1 ] = dest[head];
+      dest[head] = tmp;
+      
+    }
+  return (dest);
+}
+
+/**
+ * write_base - sends characters to be written on standard output
+ * @str: String to parse
+ */
+void write_base(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+		_putchar(str[i]);
+}
+
+
+/**
+ * base_len - Calculates the length for an octal number
+ * @num: The number for which the length is being calculated
+ * @base: Base to be calculated by
+ * Return: An integer representing the length of a number
+ */
+unsigned int base_len(unsigned int num, int base)
+{
+	unsigned int i;
+
+	for (i = 0; num > 0; i++)
+	{
+		num = num / base;
+	}
+	return (i);
+}
