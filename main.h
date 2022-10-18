0c8d7a99084f3f59df70c507f72c7341b6c329aa
Abdullah Clement Abdul Shakur <abdulshakuraclement@yahoo.com>
2022-10-18 15:34:47 -0700

    added b_changes.c

--- main.h
+++ main.h
@@ -33,6 +33,10 @@ int p_str(va_list);
 int rot13(va_list);
 int unsigned_int(va_list);
 int p_str_rev(va_list arg);
+int p_binary(va_list);
+int p_octal(va_list list);
+int p_hex(va_list list);
+
 
 
 
